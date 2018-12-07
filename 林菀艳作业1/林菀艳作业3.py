4c4badcfb83528f41067a0da2d71dde10818d34f
aikermerry <1139513692@qq.com>
2018-12-07 19:40:38 +0800

    ok

--- 林菀艳作业1/林菀艳作业3.py
+++
@@ -1,7 +0,0 @@
-a = input('grades=')
-if(a>='90'):
-	print('A')
-elif(a<='89' and a>='60'):
-	print('B')
-else:
-	print('C')
--- 林菀艳作业1/林菀艳作业4.py
+++
@@ -1,8 +0,0 @@
-def reverse(s):
-	if s == "":
-		return s 
-	else:
-		return reverse(s[1:]) +s[0]
-
-str = input('请输入一个字符串：')
-print(reverse(str))
